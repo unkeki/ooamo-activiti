1cc812fe75704170efaec1aa15b2ce3b205e8629
shenzhanwang <765410342@qq.com>
2021-04-22 09:59:23 +0800

    sql脚本更新

--- activiti.sql
+++ activiti.sql
@@ -1,214 +1,175 @@
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
---
--- Host: localhost    Database: activiti
--- ------------------------------------------------------
--- Server version	5.7.13-log
+/*
+ Navicat Premium Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+ Source Server         : 5.7
+ Source Server Type    : MySQL
+ Source Server Version : 50731
+ Source Host           : 172.16.26.113:3305
+ Source Schema         : activiti
 
---
--- Table structure for table `leaveapply`
---
+ Target Server Type    : MySQL
+ Target Server Version : 50731
+ File Encoding         : 65001
 
+ Date: 22/04/2021 09:58:43
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for leaveapply
+-- ----------------------------
 DROP TABLE IF EXISTS `leaveapply`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `leaveapply` (
+CREATE TABLE `leaveapply`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `process_instance_id` varchar(45) DEFAULT NULL,
-  `user_id` varchar(20) DEFAULT NULL,
-  `start_time` varchar(45) DEFAULT NULL,
-  `end_time` varchar(45) DEFAULT NULL,
-  `leave_type` varchar(45) DEFAULT NULL,
-  `reason` varchar(400) DEFAULT NULL,
-  `apply_time` varchar(100) DEFAULT NULL,
-  `reality_start_time` varchar(45) DEFAULT NULL,
-  `reality_end_time` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `process_instance_id` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `user_id` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `start_time` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `end_time` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `leave_type` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `reason` varchar(400) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `apply_time` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `reality_start_time` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `reality_end_time` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `leaveapply`
---
+-- ----------------------------
+-- Records of leaveapply
+-- ----------------------------
 
-LOCK TABLES `leaveapply` WRITE;
-/*!40000 ALTER TABLE `leaveapply` DISABLE KEYS */;
-/*!40000 ALTER TABLE `leaveapply` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `permission`
---
-
+-- ----------------------------
+-- Table structure for permission
+-- ----------------------------
 DROP TABLE IF EXISTS `permission`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `permission` (
+CREATE TABLE `permission`  (
   `pid` int(11) NOT NULL AUTO_INCREMENT,
-  `permissionname` varchar(45) NOT NULL,
-  PRIMARY KEY (`pid`),
-  UNIQUE KEY `permissionname_UNIQUE` (`permissionname`)
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `permissionname` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`pid`) USING BTREE,
+  UNIQUE INDEX `permissionname_UNIQUE`(`permissionname`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `permission`
---
+-- ----------------------------
+-- Records of permission
+-- ----------------------------
+INSERT INTO `permission` VALUES (2, '人事审批');
+INSERT INTO `permission` VALUES (9, '出纳付款');
+INSERT INTO `permission` VALUES (8, '总经理审批');
+INSERT INTO `permission` VALUES (22, '收货确认');
+INSERT INTO `permission` VALUES (17, '调整申请');
+INSERT INTO `permission` VALUES (21, '调整采购申请');
+INSERT INTO `permission` VALUES (3, '财务审批');
+INSERT INTO `permission` VALUES (1, '部门领导审批');
+INSERT INTO `permission` VALUES (18, '采购经理审批');
+INSERT INTO `permission` VALUES (16, '销假');
 
-LOCK TABLES `permission` WRITE;
-/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
-INSERT INTO `permission` VALUES (2,'人事审批'),(9,'出纳付款'),(8,'总经理审批'),(3,'财务审批'),(1,'部门领导审批'),(15,'采购审批');
-/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `purchase`
---
-
+-- ----------------------------
+-- Table structure for purchase
+-- ----------------------------
 DROP TABLE IF EXISTS `purchase`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `purchase` (
+CREATE TABLE `purchase`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `itemlist` text NOT NULL,
-  `total` float NOT NULL,
-  `applytime` varchar(45) DEFAULT NULL,
-  `applyer` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `itemlist` text CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `total` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `applytime` datetime(0) NULL DEFAULT NULL,
+  `applyer` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `processinstanceid` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `purchase`
---
+-- ----------------------------
+-- Records of purchase
+-- ----------------------------
 
-LOCK TABLES `purchase` WRITE;
-/*!40000 ALTER TABLE `purchase` DISABLE KEYS */;
-/*!40000 ALTER TABLE `purchase` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `role`
---
-
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
 DROP TABLE IF EXISTS `role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `role` (
+CREATE TABLE `role`  (
   `rid` int(11) NOT NULL AUTO_INCREMENT,
-  `rolename` varchar(45) NOT NULL,
-  PRIMARY KEY (`rid`),
-  UNIQUE KEY `rolename_UNIQUE` (`rolename`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `rolename` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`rid`) USING BTREE,
+  UNIQUE INDEX `rolename_UNIQUE`(`rolename`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 19 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `role`
---
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (18, '普通用户');
+INSERT INTO `role` VALUES (17, '管理员');
 
-LOCK TABLES `role` WRITE;
-/*!40000 ALTER TABLE `role` DISABLE KEYS */;
-INSERT INTO `role` VALUES (9,'人事'),(10,'出纳员'),(11,'总经理'),(16,'财务管理员'),(1,'部门经理'),(13,'采购经理');
-/*!40000 ALTER TABLE `role` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `role_permission`
---
-
+-- ----------------------------
+-- Table structure for role_permission
+-- ----------------------------
 DROP TABLE IF EXISTS `role_permission`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `role_permission` (
+CREATE TABLE `role_permission`  (
   `rpid` int(11) NOT NULL AUTO_INCREMENT,
   `roleid` int(11) NOT NULL,
   `permissionid` int(11) NOT NULL,
-  PRIMARY KEY (`rpid`),
-  KEY `a_idx` (`roleid`),
-  KEY `b_idx` (`permissionid`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`rpid`) USING BTREE,
+  INDEX `a_idx`(`roleid`) USING BTREE,
+  INDEX `b_idx`(`permissionid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 64 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `role_permission`
---
+-- ----------------------------
+-- Records of role_permission
+-- ----------------------------
+INSERT INTO `role_permission` VALUES (49, 17, 2);
+INSERT INTO `role_permission` VALUES (50, 17, 9);
+INSERT INTO `role_permission` VALUES (51, 17, 8);
+INSERT INTO `role_permission` VALUES (52, 17, 22);
+INSERT INTO `role_permission` VALUES (53, 17, 17);
+INSERT INTO `role_permission` VALUES (54, 17, 21);
+INSERT INTO `role_permission` VALUES (55, 17, 3);
+INSERT INTO `role_permission` VALUES (56, 17, 1);
+INSERT INTO `role_permission` VALUES (57, 17, 18);
+INSERT INTO `role_permission` VALUES (58, 17, 16);
+INSERT INTO `role_permission` VALUES (59, 18, 9);
+INSERT INTO `role_permission` VALUES (60, 18, 22);
+INSERT INTO `role_permission` VALUES (61, 18, 17);
+INSERT INTO `role_permission` VALUES (62, 18, 21);
+INSERT INTO `role_permission` VALUES (63, 18, 16);
 
-LOCK TABLES `role_permission` WRITE;
-/*!40000 ALTER TABLE `role_permission` DISABLE KEYS */;
-INSERT INTO `role_permission` VALUES (27,9,2),(28,10,9),(34,11,2),(35,11,8),(36,11,1),(37,13,15),(39,16,3),(42,1,3);
-/*!40000 ALTER TABLE `role_permission` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user`
---
-
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
 DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+CREATE TABLE `user`  (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) COLLATE utf8_bin NOT NULL,
-  `password` varchar(45) COLLATE utf8_bin NOT NULL,
-  `tel` varchar(45) COLLATE utf8_bin DEFAULT NULL,
-  `age` int(11) DEFAULT NULL,
-  PRIMARY KEY (`uid`),
-  UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=36 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `username` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `tel` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  `age` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`uid`) USING BTREE,
+  UNIQUE INDEX `username_UNIQUE`(`username`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 36 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `user`
---
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (31, 'xiaomi', '1234', '110', 20);
+INSERT INTO `user` VALUES (33, 'jon', '1234', '123', 23);
+INSERT INTO `user` VALUES (34, 'xiaocai', '1234', '111', 32);
+INSERT INTO `user` VALUES (35, 'WANG', '1234', '222', 33);
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (31,'xiaomi','1234','110',20),(33,'jon','1234','123',23),(34,'xiaocai','1234','111',32),(35,'WANG','1234','222',33);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_role`
---
-
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_role` (
+CREATE TABLE `user_role`  (
   `urid` int(11) NOT NULL AUTO_INCREMENT,
   `userid` int(11) NOT NULL,
   `roleid` int(11) NOT NULL,
-  PRIMARY KEY (`urid`)
-) ENGINE=InnoDB AUTO_INCREMENT=93 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  PRIMARY KEY (`urid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 98 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
---
--- Dumping data for table `user_role`
---
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES (93, 31, 17);
+INSERT INTO `user_role` VALUES (94, 33, 18);
+INSERT INTO `user_role` VALUES (96, 35, 18);
+INSERT INTO `user_role` VALUES (97, 34, 17);
 
-LOCK TABLES `user_role` WRITE;
-/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
-INSERT INTO `user_role` VALUES (47,33,9),(48,33,1),(81,31,9),(82,31,10),(83,31,16),(84,31,1),(85,31,13),(86,35,11),(87,35,1),(92,34,1);
-/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-11-18 14:12:37
+SET FOREIGN_KEY_CHECKS = 1;
